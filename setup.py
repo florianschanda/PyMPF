7c9933814173c3cbd62610feed73d62f43330dcc
Florian Schanda <florian@schanda.org.uk>
2019-03-13 10:23:42 +0100

    Add setup for PyPI

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="pympf",
+    version="1.0.0",
+    author="Florian Schanda",
+    author_email="florian@schanda.org.uk",
+    description="An arbitrary precision IEEE-754 implementation in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/florianschanda/PyMPF",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Emulators"
+    ],
+)
